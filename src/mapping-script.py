78675db43467b4238f50639bb54928090a9d4cb5
Katie Yip <ktay369@gmail.com>
2023-08-07 11:43:09 -0700

    initial commit

---
+++ src/mapping-script.py
@@ -0,0 +1,50 @@
+import json
+import uuid
+import os
+
+# Get the directory of the current script
+# script_dir = os.path.dirname(os.path.abspath(__file__))
+# print(script_dir)
+
+# Read the input JSON file
+input_file_path = '../input/metadata.json'
+output_file_path = 'mapping.json'
+
+# Check if the output file already exists
+if os.path.exists(output_file_path):
+    raise Exception(f"Output file '{output_file_path}' already exists. Script processing terminated.")
+
+
+with open(input_file_path, 'r') as input_file:
+    json_data = json.load(input_file)
+
+# Create a new list to hold the new entries
+new_entries = []
+
+# Loop through each entry in the JSON and add a new key-value pair
+# for entry in json_data:
+#     user_id = entry['id']
+#     uuid = str(uuid.uuid4())
+#     entry[user_id] = uuid
+
+# Loop through each entry in the input JSON data
+for entry in json_data["signals"]:
+    # Create a new entry with the desired fields
+    try:
+        # Attempt to get the user_id from the entry
+        user_id = entry["id"]
+    except KeyError:
+        # Handle the case where "id" field is missing in the entry
+        raise Exception("Error: 'id' field not found in an entry.")
+    unique = uuid.uuid4()
+    new_entry = {
+        user_id: str(unique)
+    }
+    # Append the new entry to the list of new entries
+    new_entries.append(new_entry)
+
+# Write the new entries to a separate JSON file
+with open(output_file_path, 'w') as output_file:
+    json.dump(new_entries, output_file, indent=4)
+
+print(f"Modified JSON written to {output_file_path}")
